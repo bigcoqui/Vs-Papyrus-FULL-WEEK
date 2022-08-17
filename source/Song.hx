3b6d9297423f5e0076dd808361a7f2b487b35a4a
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-08-17 18:51:32 -0300

    update Song.hx

--- source/Song.hx
+++ source/Song.hx
@@ -5,7 +5,7 @@ import haxe.Json;
 import haxe.format.JsonParser;
 import lime.utils.Assets;
 
-#if sys
+#if desktop
 import sys.io.File;
 import sys.FileSystem;
 #end
@@ -92,7 +92,7 @@ class Song
 		
 		var formattedFolder:String = Paths.formatToSongPath(folder);
 		var formattedSong:String = Paths.formatToSongPath(jsonInput);
-		#if MODS_ALLOWED
+		#if desktop
 		var moddyFile:String = Paths.modsJson(formattedFolder + '/' + formattedSong);
 		if(FileSystem.exists(moddyFile)) {
 			rawJson = File.getContent(moddyFile).trim();
@@ -100,7 +100,7 @@ class Song
 		#end
 
 		if(rawJson == null) {
-			#if sys
+			#if desktop
 			rawJson = File.getContent(Paths.json(formattedFolder + '/' + formattedSong)).trim();
 			#else
 			rawJson = Assets.getText(Paths.json(formattedFolder + '/' + formattedSong)).trim();
